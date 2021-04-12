2e25257baf69e1293887f5c173ebf5886116545f
Maciej Waszkielewicz <maciej.waszkielewicz@gmail.com>
2021-04-12 12:35:58 +0200

    Add slug generation and simplyfy validation

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -3,10 +3,19 @@ class ShortLink < ApplicationRecord
 
   belongs_to :user, optional: true
 
+  before_validation :generate_slug, on: :create, if: proc { |record| record.slug.nil? }
+
   validates :slug, presence: true, uniqueness: true, format: { with: /\A([a-zA-Z0-9-])+\Z/ }
-  validates :url, presence: true, format: { with: URI.regexp(['http', 'https']) }
+  validates :url, presence: true, format: { with: URI.regexp([ 'http', 'https' ]) }
 
   def to_param
     slug
+  end
+
+  def generate_slug
+    loop do
+      self.slug = (0..8).map{['-', *('a'..'z'), *('A'..'Z'), *('0'..'9')].sample}.join
+      break unless ShortLink.where(slug: self.slug).exists?
+    end
   end
 end
