ef92d8820e8b846049b697acb7656b7b15841400
Maciej Waszkielewicz <maciej.waszkielewicz@gmail.com>
2021-04-12 11:25:08 +0200

    add short link validation

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,7 +1,8 @@
 class ShortLink < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :slug, presence: true, format: { with: /\A([a-zA-Z0-9-])+\Z/ }
+  validates :slug, presence: true, uniqueness: true, format: { with: /\A([a-zA-Z0-9-])+\Z/ }
+  validates :url, presence: true, format: { with: /\A((http|https):\/\/|)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\Z/ix }
 
   def to_param
     slug
