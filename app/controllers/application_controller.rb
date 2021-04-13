e2e1a7f049da9e92965330b919918287acd6b11c
Maciej Waszkielewicz <maciej.waszkielewicz@gmail.com>
2021-04-13 08:27:21 +0200

    rescue from unauthorizated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::MimeResponds
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { render json: { message: 'Unauthorizated' }, status: 401 }
+    end
+  end
 end
